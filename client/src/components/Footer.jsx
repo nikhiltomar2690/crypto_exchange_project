1b0ea9f1c2c5fe08d465b1d3117ca729feb6cefb
Nikhil Tomar <nikhiltomar2690@gmail.com>
2023-04-23 19:26:41 +0530

    full project

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -25,7 +25,6 @@ const Footer = () => (
 
     <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
       {/* <p className="text-white text-left text-xs">@kryptomastery2022</p> */}
-      <p className="text-white text-right text-xs">All rights reserved</p>
     </div>
   </div>
 );
--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -1,26 +1,9 @@
 import { useEffect, useState } from "react";
 
 const APIKEY = import.meta.env.VITE_GIPHY_API;
-
+// in future can add gif to transaction and display each transaction happened on the platform on the home page
 const useFetch = ({ keyword }) => {
-  const [gifUrl, setGifUrl] = useState("");
-
-  const fetchGifs = async () => {
-    try {
-      const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${keyword.split(" ").join("")}&limit=1`);
-      const { data } = await response.json();
-
-      setGifUrl(data[0]?.images?.downsized_medium.url);
-    } catch (error) {
-      setGifUrl("https://metro.co.uk/wp-content/uploads/2015/05/pokemon_crying.gif?quality=90&strip=all&zoom=1&resize=500%2C284");
-    }
-  };
-
-  useEffect(() => {
-    if (keyword) fetchGifs();
-  }, [keyword]);
-
-  return gifUrl;
+  return 0;
 };
 
 export default useFetch;
